44148a497afe4d42a15e62774c545b52da2a2938
Adam Kalman <akpc556@gmail.com>
2017-02-27 10:54:59 -0600

    test configuration of sensors and motors

---
+++ adam-jordy-noah/marbleSorting.c
@@ -0,0 +1,18 @@
+#pragma config(Sensor, in1,    lineFollower,   sensorLineFollower)
+#pragma config(Sensor, in2,    potentiometer,  sensorPotentiometer)
+#pragma config(Sensor, in3,    lightSensor,    sensorReflection)
+#pragma config(Sensor, dgtl1,  limitSwitch,    sensorTouch)
+#pragma config(Sensor, dgtl2,  bumpSwitch,     sensorTouch)
+#pragma config(Sensor, dgtl3,  quad,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  sonar,          sensorSONAR_inch)
+#pragma config(Sensor, dgtl12, green,          sensorLEDtoVCC)
+#pragma config(Motor,  port2,           rightMotor,    tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port1,           flashlight,    tmotorVexFlashlight, openLoop)
+#pragma config(Motor,  port9,           servoMotor,    tmotorServoStandard, openLoop)
+#pragma config(Motor,  port10,          leftMotor,     tmotorVex393_HBridge, openLoop)
+task main()
+{
+
+
+
+}
