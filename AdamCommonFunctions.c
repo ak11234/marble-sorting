d5683f7ec6d6661d3b02f736e3e91949c5ef98a0
Adam Kalman <akpc556@gmail.com>
2017-03-03 10:41:39 -0600

    pseudocode

--- AdamCommonFunctions.c
+++ AdamCommonFunctions.c
@@ -8,6 +8,9 @@ void runMotor(tMotor myMotor, int speed, int time){
 	/*
 	Function to run a motor.
 	Takes a motor, a integer of speed, and integer of time as parameters
+	Runs that motor, at that speed, for that amount of time (in MS).
+	Example: run motor LeftMotor at a speed of 63 for 10 seconds.
+	runMotor(leftMotor, 63, 10000);
 	*/
 	int currentSpeed=motor[myMotor];
 	motor[myMotor]=speed;
