553cc549792c736adb72ab1c536eb34726c90d18
Adam Kalman <akpc556@gmail.com>
2017-03-08 10:11:17 -0600

    Updated with light sensor marble classification

--- AdamCommonFunctions.c
+++ AdamCommonFunctions.c
@@ -1,7 +1,11 @@
 /*
+@Author Adam Kalman
+
 Common functions to use in your program. To use, place this file into the same folder as your code
 and include the entire following line just before your task main
+
 #include "AdamCommonFunctions.c"
+
 */
 
 void runMotor(tMotor myMotor, int speed, int time){
@@ -17,4 +21,23 @@ void runMotor(tMotor myMotor, int speed, int time){
 	wait1Msec(time);
 	motor[myMotor]=currentSpeed;
 
+}
+int classifyMarble(int senseVal){
+	/*
+	Function to figure out what marble you have over a light sensor.
+	To use, pass it an integer of the light sensor value, and it will return a number 0-3.
+	That 0,1,2,3 represents which marble you have.
+	See code below to see which number corresponds to which marble.
+	*/
+		int marbleLightValues[3]; //You should test your own values. These numbers worked for OUR machine, but it might not for yours
+		marbleLightValues[0]=38; //Clear marble
+		marbleLightValues[1]=775; //Metal
+		marbleLightValues[2]=978; //Black
+		for (int k=0; k<=2; k++){
+			if (senseVal>marbleLightValues[k]-100 && senseVal<marbleLightValues[k]+100){
+				return k;
+			}
+	}
+
+		return 3; //No match
 }
